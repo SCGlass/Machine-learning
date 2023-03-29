74737ed980d77d3f5446c963712fa4172ea6a1c8
Sam Glass <sam.charles.glass@gmail.com>
2023-03-29 15:44:36 +0200

    completed my .py model file. Now to comment everything and write up descriptions

--- Labb/production_model.py
+++ Labb/production_model.py
@@ -1,6 +1,28 @@
 import pandas as pd
 import joblib
+from sklearn.metrics import classification_report
 
-test_data = 
+test_data =  "data\Train_data_disease.csv"
+df_test = pd.read_csv(test_data)
 
-model = joblib.load("voting_classifier.pkl")
+X,y = df_test.drop("cardio", axis = "columns"), df_test["cardio"]
+
+model = joblib.load("Labb/voting_clf_model.pkl")
+
+y_pred = model.predict(X)
+
+proba = model.predict_proba(X)
+
+proba_class_0 = proba[:,0]
+proba_class_1 = proba[:,1]
+
+data = {"probability class 0" : proba_class_0,
+        "probability class 1" : proba_class_1,
+        "prediction" : y_pred}
+
+df_predictions = pd.DataFrame(data)
+
+df_predictions.to_csv("data\predictions.csv", index=False)
+
+print(f"Accuracy score:  {model.score(X,y)}" )
+print(classification_report(y_pred, y))
